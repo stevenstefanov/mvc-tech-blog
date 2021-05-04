800cbdb37df5dee944d3cc0e64486bcf83cd8628
Steven Stefanov <77702697+stevenstefanov@users.noreply.github.com>
2021-05-03 20:38:11 -0400

    updated line structure in postRoutes.js

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -17,10 +17,7 @@ router.post('/', withAuth, async (req, res) => {
 router.delete('/:id', withAuth, async (req, res) => {
   try {
     const postData = await Post.destroy({
-      where: {
-        id: req.params.id,
-        user_id: req.session.user_id,
-      },
+      where: { id: req.params.id, user_id: req.session.user_id }
     });
 
     if (!postData) {
