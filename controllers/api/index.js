6ec07f4b8b61fcd3a88c60a79e4b18c2444888a6
Steven Stefanov <77702697+stevenstefanov@users.noreply.github.com>
2021-05-03 20:05:48 -0400

    pushing api/index.js file with section comments.

---
+++ controllers/api/index.js
@@ -0,0 +1,11 @@
+// Declaring dependencies
+const router = require("express").Router();
+const userRoutes = require("./userRoutes");
+const postRoutes = require("./postRoutes");
+
+// Set up routes
+router.use("/users", userRoutes);
+router.use("/posts", postRoutes);
+
+// Exports
+module.exports = router;
