49460abcb64d037c791e0e0cb135fb59a57f1d53
Steven Stefanov <77702697+stevenstefanov@users.noreply.github.com>
2021-05-06 18:13:14 -0400

    corrected api paths in several files. Also, corrected database descriptions in several places.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -22,7 +22,7 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/post/:id', async (req, res) => {
+router.get('/posts/:id', async (req, res) => {
   try {
     const postData = await Post.findByPk(req.params.id, {
       include: [{ model: User, attributes: ['name'] }]
--- models/Post.js
+++ models/Post.js
@@ -38,7 +38,7 @@ Post.init(
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'project',
+    modelName: 'post',
   }
 );
 
